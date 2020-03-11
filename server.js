d6458d8aa78900a75e062cdcfe2ed3d1140f32a9
PrabhuRP <52042606+PrabhuRP@users.noreply.github.com>
2020-03-11 17:12:55 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(3000, '0.0.0.0');
+console.log(`Web Server Running....`);
